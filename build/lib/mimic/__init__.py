747faeceed0dc33d885fdbbf54da8a55574a0968
Karthik-Sarvan <karthiks1mail@gmail.com>
2026-05-07 16:09:55 +0530

    Refactor code structure for improved readability and maintainability

--- build/lib/mimic/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bridge import MimicBridge
-from .sensors.mpu6050 import MPU6050Simulator
-
-__all__ = ['MimicBridge', 'MPU6050Simulator']
--- build/lib/mimic/sensors/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mpu6050 import MPU6050Simulator
-
-__all__ = ['MPU6050Simulator']
